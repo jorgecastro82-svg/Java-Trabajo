fadd288a02772a076cfa5754005096e1a6739478
Jorge Uziel <jorge.castro82@uabc.edu.mx>
2026-04-24 12:13:56 -0700

    gestor mascota

--- Desplazable.java
+++ Desplazable.java
@@ -2,4 +2,6 @@ public interface Desplazable {
     public boolean get_pushed(double x, double y);
     public double get_x();
     public double get_y();
+    public double set_x(double x);
+    public double set_y(double y);
 }
--- Portal.java
+++ Portal.java
@@ -1,7 +1,8 @@
 public class Portal implements Transportador {
     @Override
     public boolean transportar_a(Desplazable entity) {
-        // TODO Auto-generated method stub
-        return false;
+        entity.set_x(entity.get_x() + 10);
+        entity.set_y(entity.get_y() + 10);
+        return true;
     }
 }
