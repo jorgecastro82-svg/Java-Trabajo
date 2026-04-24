fa5e52289143617d01a0d5c048a74bf87bf842c1
Daniri98 <daniel.rivera98@uabc.edu.mx>
2026-04-24 19:02:33 +0000

    C

--- Cofre.java
+++ Cofre.java
@@ -1,3 +1,13 @@
 public class Cofre implements DropLoot,Danable extends EntidadEstatica{
-    
+    Cofre(int v,int vm,String n){
+        vida=v;
+        vidamax=vm;
+        nombre=n;
+    }
+    dispInfo(){
+
+    }
+    disp_hp(){
+        System.out.prinln("hp"+vida)
+    }
 }
